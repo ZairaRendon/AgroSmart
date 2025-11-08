a5eb037f0d7cd98dbbcf76221b8a5d89e960e99c
zaira <zaira.ser7@gmail.com>
2025-11-07 19:46:24 -0600

    Agrego splashScreen y Login

---
+++ app/src/main/java/com/example/agrosmart/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.agrosmart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    public static int tiempoCarga = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, Login.class);
+            startActivity(intent);
+            finish();
+        }, tiempoCarga);
+    }
+}
